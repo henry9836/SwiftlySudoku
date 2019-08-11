c8c64830fffe2c3336ee81139ae8e2695ae0bf2a
henry9836 <henry983615@gmail.com>
2019-08-11 20:45:09 +1200

    draw grid

--- main.swift
+++ main.swift
@@ -1,1 +1,23 @@
-//code here
+import Foundation
+
+var grid = Array(repeating: Array(repeating: 0, count: 9), count: 9)
+
+func displayGrid(){
+    for y in 0..<9 {
+        for x in 0..<9 {
+            if (x == 3 || x == 6){
+                print("|", terminator:"");
+            }
+            print(grid[y][x], terminator:"");
+        }
+        if (y == 2 || y == 5){
+            print("\n---+---+---");
+        }
+        else{
+            print("");
+        }        
+    }
+} 
+
+
+displayGrid();
