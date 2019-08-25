434c9d126d2155a4806b59cffe418607baaf4711
Henry <henry983615@gmail.com>
2019-08-25 21:34:44 +1200

    Ready to implement solver

--- SwiftlySudoku/main.swift
+++ SwiftlySudoku/main.swift
@@ -19,10 +19,79 @@ func displayGrid(){
     }
 }
 
+func gridSolved() -> Bool{
+    var result = true;
+
+    for y in 0..<9 {
+        for x in 0..<9 {
+            if (grid[y][x] == "0"){
+                result = false;
+            }
+        }
+    }
+
+    return result;
+}
+
+func ResetGrid(){
+	for y in 0..<9 {
+        for x in 0..<9 {
+			grid[y][x] = "0";
+		}
+	}
+}
+
 func Clear(){
     for _ in 0..<100 {
         print("\n")
     }
+}
+
+func randomFill() -> Bool{
+	return true;
+}
+
+func predefinedFill() -> Bool{
+
+	//rows (y,x)
+	//first row
+	grid[0][0] = "8";
+	grid[0][3] = "4";
+	grid[0][5] = "6";
+	grid[0][8] = "7";
+	//second row
+    grid[1][6] = "4";
+    //third row
+    grid[2][1] = "1";
+    grid[2][6] = "6";
+    grid[2][7] = "5";
+    //fourth row
+    grid[3][0] = "5";
+    grid[3][2] = "9";
+    grid[3][4] = "3";
+    grid[3][6] = "7";
+    grid[3][7] = "8";
+    //fifth row
+    grid[4][4] = "7";
+    //sixth row
+    grid[5][1] = "4";
+    grid[5][2] = "8";
+    grid[5][4] = "2";
+    grid[5][6] = "1";
+    grid[5][8] = "3";
+    //seventh row
+    grid[6][1] = "5";
+    grid[6][2] = "2";
+    grid[6][7] = "9";
+    //eigth row
+    grid[7][2] = "1";
+    //ninth row
+    grid[8][0] = "3";
+    grid[8][3] = "9";
+    grid[8][5] = "2";
+    grid[8][8] = "5";
+
+	return true;
 }
 
 func userFill() -> Bool{
@@ -73,7 +142,7 @@ func userFill() -> Bool{
                     print(i)
                     print(j)
                     print(String(userGrid[i][userGrid[i].index(userGrid[i].startIndex, offsetBy: j)]))
-                    if let tester = Int(String(userGrid[i][userGrid[i].index(userGrid[i].startIndex, offsetBy: j)])){
+                    if let _ = Int(String(userGrid[i][userGrid[i].index(userGrid[i].startIndex, offsetBy: j)])){
                         
                     }
                     else{
@@ -108,13 +177,56 @@ func userFill() -> Bool{
     return true;
 }
 
+func Solve() -> Bool{
 
-displayGrid();
-var userRes = userFill();
-Clear();
-if userRes{
-    displayGrid();
+    if (gridSolved()){
+        return true;
+    }
+
+    //Final Check
+    if (gridSolved()){
+        return true;
+    }    
+
+    return false;
 }
-else{
-    print("\nSomething went wrong exitting...")
+
+while true{
+	Clear();
+
+	//empty grid
+	ResetGrid();
+
+	print ("-= Sudoku Solver =- \n 1. User Defined Board\n 2. Randomly Generated Board\n 3. Solve Predefined Problem\n 99. Exit");
+	//displayGrid();
+	let c = readLine();
+	var boardCreated = false;
+	if (c == "1"){
+		boardCreated = userFill();
+	}
+	else if (c == "2"){
+		boardCreated = randomFill();
+	}
+	else if (c == "3"){
+		boardCreated = predefinedFill();
+	}
+    else if (c == "99"){
+        break
+    }
+	else{
+		print("\nInput was incorrect")
+	}
+	if boardCreated{
+		displayGrid();
+        let solvedResult = Solve();
+        if (solvedResult){
+            print("Solution Found!")
+        }
+        else{
+            print("Could not find solution :(")
+        }
+	}
+    print("Press Enter To Continue")
+	let _ = readLine();
+	
 }
